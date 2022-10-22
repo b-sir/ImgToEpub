ab5e62409a10f8e7a77172719dccd285e2aed3d1
赵璧 <zhaobi@pandadastudio.com>
2022-10-22 16:08:55 +0800

    生成epub文件的基础流程

--- commicImg2Epub.py
+++ commicImg2Epub.py
@@ -1,15 +1,63 @@
+#!/usr/bin/python3
+from genericpath import isdir
 import os
 from shutil import copyfile
+from typing import Text
 from PIL import Image
+import zipfile
 
 #需要 pip3 install Pillow
-#需要 
+
+#################-----Config-----###############
+BOOK_TITLE = "死神"
+BOOK_ID = "3WMTMZLG-COKT-YK48-S57I-BOFZHLV7VLB1" #瞎填一个吧，懒得写随机函数了
+#################-----Config-----###############
+
 
 CurrentDir = os.path.abspath(os.path.dirname(__file__))
 SrcPath = os.path.join(CurrentDir, "Resources")
 TgtPath = os.path.join(CurrentDir, "Output")
 
+SpliteImage = True #切割宽比高大的图片为两张图片
 ReadModeRight = True #设置切割图片的阅读顺序 True为日漫右向左读
+
+#临时目录
+TmpPath = os.path.join(TgtPath, "Temp")
+if not os.path.exists(TmpPath):
+    os.mkdir(TmpPath)
+
+# META-INF目录
+if not os.path.exists(os.path.join(TmpPath, "META-INF")):
+    os.mkdir(os.path.join(TmpPath, "META-INF"))
+
+# OEBPS目录
+if not os.path.exists(os.path.join(TmpPath, "OEBPS")):
+    os.mkdir(os.path.join(TmpPath, "OEBPS"))
+
+# OEBPS/image目录
+ImgFolder = os.path.join(TmpPath, "OEBPS", "image")
+if not os.path.exists(ImgFolder):
+    os.mkdir(ImgFolder)
+
+# OEBPS/style目录
+if not os.path.exists(os.path.join(TmpPath, "OEBPS", "style")):
+    os.mkdir(os.path.join(TmpPath, "OEBPS", "style"))
+
+# OEBPS/text目录
+TextFolder = os.path.join(TmpPath, "OEBPS", "text")
+if not os.path.exists(TextFolder):
+    os.mkdir(TextFolder)
+
+def writeFile(path, str):
+    fp = open(path, 'w', encoding='UTF-8')
+    fp.write(str)
+    fp.close()
+
+def readFile(path):
+    fp = open(path, 'r', encoding='UTF-8')
+    str = fp.read()
+    fp.close()
+    return str
 
 if not os.path.exists(TgtPath):
     os.mkdir(TgtPath)
@@ -22,10 +70,14 @@ def walk(folder, rootData, chaperData):
             pFormat = fl[-4:]
             if pFormat == ".png" or pFormat == ".jpg":
                 chaperData.append(tmp_path)
+            else:
+                print("异常文件 "+fl)
         else:
             newChapter = {}
             newChapter["name"] = fl
             newChapter["files"] = []
+            newChapter["newTextfiles"] = []
+            newChapter["newImagefiles"] = []
             rootData.append(newChapter)
             walk(tmp_path, rootData, newChapter["files"])
 
@@ -33,23 +85,33 @@ srcData = []
 rootFiles = []
 walk(SrcPath, srcData, rootFiles)
 
-TmpPath = os.path.join(TgtPath, "Temp")
-if not os.path.exists(TmpPath):
-    os.mkdir(TmpPath)
 
+textTempStr = readFile(os.path.join(CurrentDir, "libs", "OEBPS", "text", "p_temp.xhtml"))
+def genOEBPSTextFile(chapterID, subID, imageName, data):
+    newTextStr = textTempStr.replace("###BOOK_TITLE###", BOOK_TITLE, 1)
+    newTextStr = newTextStr.replace("###IMAGE_FILE###", imageName, 1)
+    fileName = "p%03d_%05d"%(chapterID,subID)
+    writeFile(os.path.join(TextFolder, fileName+".xhtml"), newTextStr)
+
+    data["newTextfiles"].append(fileName)
+    data["newImagefiles"].append(imageName)
+
+
+# 处理图片、并统计数据
 print("----------开始处理原始图片，并迁移到临时目录-----------")
-for data in srcData:
-    tmpFolder = os.path.join(TmpPath, data["name"])
-    if not os.path.exists(tmpFolder): #创建对应章节的临时文件夹
-        os.mkdir(tmpFolder)
-    
-    print("处理章节："+data["name"])
 
-    index = 1
+
+chapterID = 0
+for data in srcData:
+    print("处理章节："+data["name"])
+    chapterID = chapterID + 1
+    data["chapterID"] = chapterID
+    subID = 1
+
     for file in data["files"]:
         img0 = Image.open(file)
         w, h = img0.size
-        if w > h: #宽>高 需要切隔为两张图片
+        if SpliteImage and w > h: #宽>高 需要切隔为两张图片
             box1 = (w-w//2, 0, w, h)
             box2 = (0, 0, w//2, h)
             if not ReadModeRight:
@@ -58,11 +120,106 @@ for data in srcData:
             newImg1 = img0.crop(box1)
             newImg2 = img0.crop(box2)
 
-            newImg1.save(os.path.join(tmpFolder, ("%04d.png"%index)), "png")
-            index = index + 1
-            newImg2.save(os.path.join(tmpFolder, ("%04d.png"%index)), "png")
-            index = index + 1
+            name1 = "i%03d_%05d.png"%(chapterID,subID)
+            newImg1.save(os.path.join(ImgFolder, name1), "png")
+            genOEBPSTextFile(chapterID,subID,name1,data)
+            subID = subID + 1
+            name2 = "i%03d_%05d.png"%(chapterID,subID)
+            newImg2.save(os.path.join(ImgFolder, name2 ), "png")
+            genOEBPSTextFile(chapterID,subID,name2,data)
+            subID = subID + 1
+
         else:
             pFormat = file[-4:]
-            copyfile(file, os.path.join(tmpFolder, ("%04d"%index) + pFormat))
-            index = index + 1
+            name = "i%03d_%05d%s"%(chapterID,subID,pFormat)
+            copyfile(file, os.path.join(ImgFolder, name))
+            genOEBPSTextFile(chapterID,subID,name,data)
+            subID = subID + 1
+
+#封面图片处理
+coverSrcFile = ""
+if len(rootFiles) > 0:
+    coverSrcFile = rootFiles[0]
+else:
+    coverSrcFile = os.path.join(ImgFolder, srcData[0]["newImagefiles"][0])
+coverFormat = coverSrcFile[-4:]
+coverFileName = "cover"+coverFormat 
+copyfile(coverSrcFile, os.path.join(ImgFolder, coverFileName))
+
+#print("图片处理结束，请打开")
+#print("https://github.com/wing-kai/epub-manga-creator")
+#print("https://wing-kai.github.io/epub-manga-creator/")
+
+# mimetype固定文件
+copyfile(os.path.join(CurrentDir, "libs", "mimetype"), os.path.join(TmpPath, "mimetype"))
+
+# 固定内容 指向standard.opf
+copyfile(os.path.join(CurrentDir, "libs", "META-INF", "container.xml"), os.path.join(TmpPath, "META-INF", "container.xml"))
+
+# css固定文件(对漫画不重要)
+copyfile(os.path.join(CurrentDir, "libs", "OEBPS", "style", "fixed-layout-jp.css"), os.path.join(TmpPath, "OEBPS", "style", "fixed-layout-jp.css"))
+
+
+# 生成opf文件内容
+coverFormatDesc = "image/png" if pFormat == ".png" else "image/jpeg"
+itemsStr = '<item id="cover" href="image/%s" media-type="%s" properties="cover-image"></item>\n'%(coverFileName, coverFormatDesc)
+itemsStr += '<item id="p_cover" href="text/p_cover.xhtml" media-type="application/xhtml+xml" properties="svg" fallback="cover"></item>\n'
+itemRefStr = ""
+dictStr = ""
+
+isInLeft = False
+for data in srcData:
+    chapterID = data["chapterID"]
+
+    dictStr = dictStr + '<li><a href="text/p%03d_00001.xhtml">%s</a></li>\n'%(chapterID, data["name"])
+
+    for imageItem in data["newImagefiles"]:
+        pFormat = imageItem[-4:]
+        pFormatDesc = "image/png" if pFormat == ".png" else "image/jpeg"
+        
+        itemsStr = itemsStr + '<item id="%s" href="image/%s" media-type="%s"></item>\n'%(imageItem[:-4], imageItem, pFormatDesc)
+
+    for i in range(len(data["newTextfiles"])):
+        textItem = data["newTextfiles"][i]
+        imageItem = data["newImagefiles"][i]
+        
+        itemsStr = itemsStr + '<item id="%s" href="text/%s.xhtml" media-type="application/xhtml+xml" properties="svg" fallback="%s"></item>\n'%(textItem, textItem, imageItem[:-4])
+        
+        properties = "page-spread-left" if isInLeft else "page-spread-right"
+        itemRefStr = itemRefStr + '<itemref linear="yes" idref="%s" properties="%s"></itemref>\n'%(textItem, properties)
+        isInLeft = not isInLeft
+
+opfTempStr = readFile(os.path.join(CurrentDir, "libs", "OEBPS", "standard.opf"))
+newOpfStr = opfTempStr.replace("###BOOK_TITLE###", BOOK_TITLE)
+newOpfStr = newOpfStr.replace("###BOOK_ID###", BOOK_ID)
+newOpfStr = newOpfStr.replace("###BOOK_ITEMS###", itemsStr)
+newOpfStr = newOpfStr.replace("###BOOK_ITEMREFS###", itemRefStr)
+
+writeFile(os.path.join(TmpPath, "OEBPS", "standard.opf"), newOpfStr)
+
+
+# 生成 navigation-documents.xhtml 内容
+
+docTempStr = readFile(os.path.join(CurrentDir, "libs", "OEBPS", "navigation-documents.xhtml"))
+newDocStr = docTempStr.replace("###BOOK_DICT###", dictStr)
+writeFile(os.path.join(TmpPath, "OEBPS", "navigation-documents.xhtml"), newDocStr)
+
+
+### 文件准备完毕，打zip包
+def zipToFile(zipFile, folder, root):
+    preLen = len(root)
+    fs = os.listdir(folder)
+    # 必须先处理文件、再处理文件夹
+    for fl in fs:
+        tmp_path = os.path.join(folder, fl)
+        if not os.path.isdir(tmp_path):
+            arcname = tmp_path[preLen:].strip(os.path.sep)
+            zipFile.write(tmp_path, arcname)
+    for fl in fs:
+         tmp_path = os.path.join(folder, fl)
+         if os.path.isdir(tmp_path):
+            zipToFile(zipFile, tmp_path, root)
+
+
+bookFile = zipfile.ZipFile(os.path.join(TgtPath, "Book.epub"), "w")
+zipToFile(bookFile, TmpPath, TmpPath)
