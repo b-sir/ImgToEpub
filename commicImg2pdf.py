04f5875c796bd59bdc28f398401ac25294aa30eb
赵璧 <zhaobi@pandadastudio.com>
2023-05-25 20:20:46 +0800

    Imgs To PDF (使用borb库)

--- commicImg2pdf.py
+++ commicImg2pdf.py
@@ -1,13 +1,19 @@
 #!/usr/bin/python3
-from functools import cache
-from genericpath import isdir
 import os,sys
-import uuid
 from shutil import copyfile, rmtree
-from typing import Text
-from PIL import Image
-import zipfile
 
+from pathlib import Path
+from borb.pdf import Document
+from borb.pdf import Page
+from borb.pdf import SingleColumnLayout
+from borb.pdf import Paragraph
+from borb.pdf import PDF
+from borb.pdf import Image as PdfImage
+from decimal import Decimal
+
+from PIL import Image
+
+#需要 pip3 install borb
 #需要 pip3 install Pillow
 
 #################-----Config-----###############
@@ -24,21 +30,7 @@ SrcPath = os.path.join(CurrentDir, "Resources")
 TgtPath = os.path.join(CurrentDir, "Output")
 
 TmpPath = os.path.join(TgtPath, "Temp")
-TextFolder = os.path.join(TmpPath, "OEBPS", "text")
 ImgFolder = os.path.join(TmpPath, "OEBPS", "image")
-
-
-def writeFile(path, str):
-    fp = open(path, 'w', encoding='UTF-8')
-    fp.write(str)
-    fp.close()
-
-def readFile(path):
-    fp = open(path, 'r', encoding='UTF-8')
-    str = fp.read()
-    fp.close()
-    return str
-
 
 def walk(folder, rootData, chaperData):
     fs = os.listdir(folder)
@@ -85,31 +77,6 @@ def PrintSrcData(srcData):
         for fileName in iData["files"]:
             print(fileName)
 
-textTempStr = readFile(os.path.join(CurrentDir, "libs", "OEBPS", "text", "p_temp.xhtml"))
-def genOEBPSTextFile(chapterID, subID, imageName, bookTitle, data):
-    newTextStr = textTempStr.replace("###BOOK_TITLE###", bookTitle, 1)
-    newTextStr = newTextStr.replace("###IMAGE_FILE###", imageName, 1)
-    fileName = "p%03d_%05d"%(chapterID,subID)
-    writeFile(os.path.join(TextFolder, fileName+".xhtml"), newTextStr)
-
-    data["newTextfiles"].append(fileName)
-    data["newImagefiles"].append(imageName)
-
-### 文件准备完毕，打zip包
-def zipToFile(zipFile, folder, root):
-    preLen = len(root)
-    fs = os.listdir(folder)
-    # 必须先处理文件、再处理文件夹
-    for fl in fs:
-        tmp_path = os.path.join(folder, fl)
-        if not os.path.isdir(tmp_path):
-            arcname = tmp_path[preLen:].strip(os.path.sep)
-            zipFile.write(tmp_path, arcname)
-    for fl in fs:
-         tmp_path = os.path.join(folder, fl)
-         if os.path.isdir(tmp_path):
-            zipToFile(zipFile, tmp_path, root)
-
 def reGenTempFolder():
     if not os.path.exists(TgtPath):
         os.mkdir(TgtPath)
@@ -117,10 +84,6 @@ def reGenTempFolder():
     #临时目录
     if not os.path.exists(TmpPath):
         os.mkdir(TmpPath)
-
-    # META-INF目录
-    if not os.path.exists(os.path.join(TmpPath, "META-INF")):
-        os.mkdir(os.path.join(TmpPath, "META-INF"))
 
     # OEBPS目录
     if not os.path.exists(os.path.join(TmpPath, "OEBPS")):
@@ -130,20 +93,14 @@ def reGenTempFolder():
     if not os.path.exists(ImgFolder):
         os.mkdir(ImgFolder)
 
-    # OEBPS/style目录
-    if not os.path.exists(os.path.join(TmpPath, "OEBPS", "style")):
-        os.mkdir(os.path.join(TmpPath, "OEBPS", "style"))
-
-    # OEBPS/text目录
-    if not os.path.exists(TextFolder):
-        os.mkdir(TextFolder)
-
+def genOEBPSTextFile(chapterID, subID, imageName, bookTitle, data):
+    data["newImagefiles"].append(imageName)
 
 def genBook(srcData, bookTitle, outFilename):
     if os.path.exists(os.path.join(TgtPath, outFilename)):
         print("跳过：已存在电子书 "+outFilename)
         return
-
+    
     if os.path.exists(TmpPath):
         rmtree(TmpPath)
     reGenTempFolder()
@@ -192,78 +149,43 @@ def genBook(srcData, bookTitle, outFilename):
                 sys.exit()
             except:
                 print("\n\n文件异常 无法处理 跳过："+file+"\n\n")
+    
+    # create an empty Document
+    pdf = Document()
 
-    #封面图片处理
-    coverSrcFile = os.path.join(ImgFolder, srcData[0]["newImagefiles"][0])
-    coverFormat = coverSrcFile[-4:]
-    coverFileName = "cover"+coverFormat 
-    copyfile(coverSrcFile, os.path.join(ImgFolder, coverFileName))
-
-    #print("图片处理结束，请打开")
-    #print("https://github.com/wing-kai/epub-manga-creator")
-    #print("https://wing-kai.github.io/epub-manga-creator/")
-
-    # mimetype固定文件
-    copyfile(os.path.join(CurrentDir, "libs", "mimetype"), os.path.join(TmpPath, "mimetype"))
-
-    # 固定内容 指向standard.opf
-    copyfile(os.path.join(CurrentDir, "libs", "META-INF", "container.xml"), os.path.join(TmpPath, "META-INF", "container.xml"))
-
-    # css固定文件(对漫画不重要)
-    copyfile(os.path.join(CurrentDir, "libs", "OEBPS", "style", "fixed-layout-jp.css"), os.path.join(TmpPath, "OEBPS", "style", "fixed-layout-jp.css"))
-
-
-    # 生成opf文件内容
-    coverFormatDesc = "image/png" if pFormat == ".png" else "image/jpeg"
-    itemsStr = '<item id="cover" href="image/%s" media-type="%s" properties="cover-image"></item>\n'%(coverFileName, coverFormatDesc)
-    itemsStr += '<item id="p_cover" href="text/p_cover.xhtml" media-type="application/xhtml+xml" properties="svg" fallback="cover"></item>\n'
-    itemRefStr = ""
-    dictStr = ""
-
-    isInLeft = False
     for data in srcData:
         chapterID = data["chapterID"]
 
-        dictStr = dictStr + '<li><a href="text/p%03d_00001.xhtml">%s</a></li>\n'%(chapterID, data["name"])
-
         for imageItem in data["newImagefiles"]:
-            pFormat = imageItem[-4:]
-            pFormatDesc = "image/png" if pFormat == ".png" else "image/jpeg"
-            
-            itemsStr = itemsStr + '<item id="%s" href="image/%s" media-type="%s"></item>\n'%(imageItem[:-4], imageItem, pFormatDesc)
+            #pFormat = imageItem[-4:]
+            #pFormatDesc = "image/png" if pFormat == ".png" else "image/jpeg"
+            fullPath = os.path.join(ImgFolder, imageItem)
+            img0 = Image.open(fullPath)
+            w, h = img0.size
 
-        for i in range(len(data["newTextfiles"])):
-            textItem = data["newTextfiles"][i]
-            imageItem = data["newImagefiles"][i]
-            
-            itemsStr = itemsStr + '<item id="%s" href="text/%s.xhtml" media-type="application/xhtml+xml" properties="svg" fallback="%s"></item>\n'%(textItem, textItem, imageItem[:-4])
-            
-            properties = "page-spread-left" if isInLeft else "page-spread-right"
-            itemRefStr = itemRefStr + '<itemref linear="yes" idref="%s" properties="%s"></itemref>\n'%(textItem, properties)
-            isInLeft = not isInLeft
+            # pageW = 842 * w / h
+            # pageH = 842
+            pageW = 1684 * w / h
+            pageH = 1684
+            # add an empty Page
+            page = Page(
+                width=Decimal(pageW+10),
+                height=Decimal(pageH+10),
+            )
+            pdf.add_page(page)
 
-    opfTempStr = readFile(os.path.join(CurrentDir, "libs", "OEBPS", "standard.opf"))
-    newOpfStr = opfTempStr.replace("###BOOK_TITLE###", bookTitle)
-    newOpfStr = newOpfStr.replace("###BOOK_ID###", str(uuid.uuid1()))
-    newOpfStr = newOpfStr.replace("###BOOK_ITEMS###", itemsStr)
-    newOpfStr = newOpfStr.replace("###BOOK_ITEMREFS###", itemRefStr)
-
-    writeFile(os.path.join(TmpPath, "OEBPS", "standard.opf"), newOpfStr)
-
-
-    # 生成 navigation-documents.xhtml 内容
-
-    docTempStr = readFile(os.path.join(CurrentDir, "libs", "OEBPS", "navigation-documents.xhtml"))
-    newDocStr = docTempStr.replace("###BOOK_DICT###", dictStr)
-    writeFile(os.path.join(TmpPath, "OEBPS", "navigation-documents.xhtml"), newDocStr)
-
+            layout = SingleColumnLayout(page, Decimal(0), Decimal(0))
+            img: PdfImage = PdfImage(
+                Path(fullPath),
+                width=Decimal(pageW),
+                height=Decimal(pageH),
+            )
+            layout.add(img)
 
     print("\n压制中... => "+outFilename)
-    bookFile = zipfile.ZipFile(os.path.join(TgtPath, outFilename), "w")
-    zipToFile(bookFile, TmpPath, TmpPath)
+    with open(Path(os.path.join(TgtPath, outFilename)), "wb") as pdf_file_handle:
+        PDF.dumps(pdf_file_handle, pdf)
 
-    if os.path.exists(TmpPath):
-        rmtree(TmpPath)
 
 print("请输入图片所在最上层目录 路径，如：")
 print(SrcPath)
@@ -276,7 +198,6 @@ if not os.path.exists(SrcPath):
 
 TgtPath = os.path.join(SrcPath, "..", "BookOutput")
 TmpPath = os.path.join(TgtPath, "Temp")
-TextFolder = os.path.join(TmpPath, "OEBPS", "text")
 ImgFolder = os.path.join(TmpPath, "OEBPS", "image")
 
 BOOK_TITLE = os.path.basename(SrcPath)
@@ -294,9 +215,12 @@ if (len(SrcData) > MAX_CHAPTER):
     while st < total:
         subData = SrcData[st:ed]
         title = "%s%d-%d"%(BOOK_TITLE, (st+1), ed)
-        genBook(subData, title, title+".epub")
+        genBook(subData, title, title+".pdf")
 
         st = ed
         ed = min(st + MAX_CHAPTER, total)
 else:
-    genBook(SrcData, BOOK_TITLE, BOOK_TITLE+".epub")
+    genBook(SrcData, BOOK_TITLE, BOOK_TITLE+".pdf")
+
+
+
